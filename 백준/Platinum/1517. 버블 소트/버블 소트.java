dcb3146dd7021296be5f52f1c9664ca5431a7216
RunSoo <109501116+RunSoo@users.noreply.github.com>
2023-03-22 21:53:43 +0900

    [Platinum V] Title: 버블 소트, Time: 496 ms, Memory: 94484 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -4,55 +4,56 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static int N;
 	public static int[] A, tmp;
 	public static long result;
 	public static void main(String[] args) throws Exception, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		A = new int[N+1];
-		tmp = new int[N+1];
+		A = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i=1; i<=N; i++) {
+		for (int i=0; i<N; i++) {
 			A[i]=Integer.parseInt(st.nextToken());
 		}
-		result=0;
-		merge_sort(1, N);
+		tmp = new int[N];
+		result = 0;
+		
+		merged_sort(0, N-1);
 		System.out.println(result);
 	}
-	private static void merge_sort(int s, int e) {
-		if (e-s<1) return;
-		
+	private static void merged_sort(int s, int e) {
+		if (e-s<1) {
+			return;
+		}
 		int m = s+(e-s)/2;
-		merge_sort(s, m);
-		merge_sort(m+1, e);
+		merged_sort(s, m);
+		merged_sort(m+1, e);
+		merge(s, m ,e);
+		
+	}
+	private static void merge(int s, int m, int e) {
 		for (int i=s; i<=e; i++) {
 			tmp[i]=A[i];
 		}
-		
+		int i=s;
 		int k=s;
-		int idx1=s;
-		int idx2=m+1;
-		while (idx1<=m && idx2<=e) {
-			if (tmp[idx1]>tmp[idx2]) {
-				A[k]=tmp[idx2];
-				result = result+idx2-k;
-				k++;
-				idx2++;
+		int j=m+1;
+		
+		while (i<=m && j<=e) {
+			if (tmp[i]>tmp[j]) {
+				A[k]=tmp[j];
+				result+= j-k;
+				k++; j++;
 			} else {
-				A[k]=tmp[idx1];
-				k++;
-				idx1++;
+				A[k]=tmp[i];
+				k++; i++;
 			}
 		}
-		while(idx1<=m) {
-			A[k]=tmp[idx1];
-			k++;
-			idx1++;
+		while (i<=m) {
+			A[k++]=tmp[i++];
 		}
-		while(idx2<=e) {
-			A[k]=tmp[idx2];
-			k++;
-			idx2++;
+		while (j<=e) {
+			A[k++]=tmp[j++];
 		}
 	}
 }
