60073c6d62cfe017106c4db8a4b2e20cea4896c9
RunSoo <109501116+RunSoo@users.noreply.github.com>
2023-04-04 11:22:59 +0900

    [Gold V] Title: ABCDE, Time: 416 ms, Memory: 35660 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,60 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	static int N;
-	static int M;
-	static boolean[] visited;
-	static int result;
-	static Map<Integer, List<Integer>> friends= new HashMap<Integer, List<Integer>>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		for (int i=0; i<N; i++) {
-			friends.put(i,new ArrayList<>());
-		}
-		for (int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			friends.get(a).add(b);
-			friends.get(b).add(a);
-		}
-		result = 0;
-		visited = new boolean[N];
-		for (int i=0; i<N; i++) {
-			if (visited[i]==false) {
-				dfs(i, 0);
-				if (result==1) { 
-					break;
-				}
-			}
-			
-		}
-		System.out.println(result);
-	}
-	private static void dfs(int person, int step) {
-		 if (step==4) {
-			 result=1;
-			 return;
-		 }
-		 
-		 if (visited[person]==true) return;
-		
-		 visited[person]=true;
-		 for (int i=0; i<friends.get(person).size(); i++) {
-			 if (visited[friends.get(person).get(i)]) continue;
-			 dfs(friends.get(person).get(i), step+1);
-		 }
-		 visited[person]=false;
-	}
-	
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M;
+    static boolean result;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        graph = new ArrayList[N];
+        visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        result = false;
+        for (int i = 0; i < N; i++) {
+            visited[i] = true;
+            dfs(i, 0);
+
+            if (result) {
+                System.out.println(1);
+                return;
+            }
+
+            visited[i] = false;
+        }
+
+        System.out.println(0);
+    }
+
+    private static void dfs(int cur, int depth) {
+        if (depth == 4) {
+            result = true;
+            return;
+        }
+
+        for (int next : graph[cur]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                dfs(next, depth + 1);
+                visited[next] = false;
+            }
+        }
+    }
 }
