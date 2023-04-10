5f20dcd96cae2d03cd69009de4aa56bfc971dabd
RunSoo <109501116+RunSoo@users.noreply.github.com>
2023-04-10 15:29:18 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15608 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,38 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
-	static Map<Integer, Integer> map = new HashMap<>(); 
 	public static void main(String[] args) throws Exception, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		
-		map.put(1, 0);
-		map.put(2, 1);
-		map.put(3, 1);
-		System.out.println(getNum(N));
-	}
-	public static int getNum(int N) {
-		if (map.containsKey(N)) {
-			return map.get(N);
-		} else {
-			int result=0;
-			if (N>3) {
-				if (N%6==0) {
-					result=Math.min(Math.min(getNum(N/2), getNum(N/3)), getNum(N-1))+1;
-				} else if (N%3==0) {
-					result=Math.min(getNum(N/3), getNum(N-1))+1;
-				} else if (N%2==0) {
-					result=Math.min(getNum(N/2), getNum(N-1))+1;
-				} else {
-					result=getNum(N-1)+1;
-				}
-			}
-			map.put(N, result);
-			return map.get(N);
+		int[] cntArr = new int[N+1];
+		cntArr[1]=0;
+		if (N>=2) {
+			cntArr[2]=1;			
 		}
+		if (N>=3) {
+			cntArr[3]=1;			
+		}
+		for (int i=4; i<=N; i++) {
+			cntArr[i]=cntArr[i-1]+1;
+			if (i%2==0 && i%3==0) cntArr[i]=Math.min(cntArr[i], Math.min(cntArr[i/2]+1, cntArr[i/3]+1));
+			else if (i%2==0) cntArr[i]=Math.min(cntArr[i], cntArr[i/2]+1);
+			else if (i%3==0) cntArr[i]=Math.min(cntArr[i], cntArr[i/3]+1);
+		}
+		System.out.println(cntArr[N]);
 	}
 }
