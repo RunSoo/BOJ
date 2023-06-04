0d12da8b1e49fb39f91a56ce05cf161f76f41470
RunSoo <109501116+RunSoo@users.noreply.github.com>
2023-06-04 21:25:58 +0900

    [Gold V] Title: 적록색약, Time: 104 ms, Memory: 12956 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -6,73 +6,82 @@ import java.util.Queue;
 
 public class Main {
 	static int N;
-	static String[][] map;
-	static boolean[][] visited;
-	static int[] dr = new int[] { -1, 1, 0, 0, };
-	static int[] dc = new int[] { 0, 0, -1, 1 };
-
-	public static void main(String[] args) throws IOException {
+	static char[][] grid;
+	static boolean[][] visited1;
+	static boolean[][] visited2;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static int dist1, dist2;
+	public static void main(String[] args) throws Exception, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new String[N][N];
-		visited = new boolean[N][N];
-		for (int n = 0; n < N; n++) {
-			String str = br.readLine();
-			for (int m = 0; m < N; m++) {
-				map[n][m] = String.valueOf(str.charAt(m));
-			}
+		grid = new char[N][N];
+		for (int r=0; r<N; r++) {
+			grid[r]=br.readLine().toCharArray();
 		}
-//		System.out.println(Arrays.deepToString(arr));
-		int ans = 0;
-		for (int n = 0; n < N; n++) {
-			for (int m = 0; m < N; m++) {
-				if (!visited[n][m]) {
-					bfs(new int[] { n, m });
-					ans++;
-				}
-			}
-		}
-		System.out.print(ans+ " ");
-		int ans1 = 0;
-		visited = new boolean[N][N];
-		for (int n = 0; n < N; n++) {
-			for (int m = 0; m < N; m++) {
-				if(map[n][m].equals("R")) {
-					map[n][m]="G";
-				}
-			}
-		}
-		for (int n = 0; n < N; n++) {
-			for (int m = 0; m < N; m++) {
-				if (!visited[n][m]) {
-					bfs(new int[] { n, m });
-					ans1++;
-				}
-			}
-		}
-		System.out.println(ans1);
+		visited1 = new boolean[N][N];
+		dist1=0;
+		normdist();
+		visited2 = new boolean[N][N];
+		dist2=0;
+		cbdist();
+		System.out.println(dist1+" "+dist2);
 	}
-
-
-
-	private static void bfs(int[] arr) {
-
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(arr);
-		visited[arr[0]][arr[1]] = true;
-		while (!queue.isEmpty()) {
-			int[] arrTmp = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int nr = arrTmp[0] + dr[i];
-				int nc = arrTmp[1] + dc[i];
-				if (nr >= 0 && nc >= 0 && nr < N && nc < N && !visited[nr][nc]
-						&& map[nr][nc].equals(map[arrTmp[0]][arrTmp[1]])) {
-					visited[nr][nc] = true;
-					queue.add(new int[] { nr, nc });
+	private static void cbdist() {
+		Queue<Integer[]> queue = new LinkedList<>();
+		for (int r=0; r<N; r++) {
+			for (int c=0; c<N; c++) {
+				if (visited2[r][c]==false) {
+					char start = grid[r][c];
+					queue.add(new Integer[] {r, c});
+					while(!queue.isEmpty()) {
+						Integer[] top = queue.poll();
+						for (int d=0; d<4; d++) {
+							int nr = top[0]+dr[d];
+							int nc = top[1]+dc[d];
+							if (nr>=0 && nc>=0 && nr<N && nc<N && visited2[nr][nc]==false) {
+								if (start=='R' || start=='G') {
+									if (grid[nr][nc]=='R'|| grid[nr][nc]=='G') {
+										visited2[nr][nc]=true;
+										queue.add(new Integer[] {nr, nc});
+									}
+								} else {
+									if (grid[nr][nc]==start) {
+										visited2[nr][nc]=true;
+										queue.add(new Integer[] {nr, nc});
+									}
+								}
+							}
+						}
+					}
+					dist2++;
 				}
-
 			}
 		}
-
+		
 	}
+	private static void normdist() {
+		Queue<Integer[]> queue = new LinkedList<Integer[]>();
+		for (int r=0; r<N; r++) {
+			for (int c=0; c<N; c++) {
+				if (visited1[r][c]==false) {
+					char start = grid[r][c];
+					queue.add(new Integer[]{r, c});
+					while(!queue.isEmpty()) {
+						Integer[] top = queue.poll();
+						for (int d=0; d<4; d++) {
+							int nr = top[0]+dr[d];
+							int nc = top[1]+dc[d];
+							if (nr>=0 && nr<N && nc>=0 && nc<N && visited1[nr][nc]==false && grid[nr][nc]==start) {
+								visited1[nr][nc]=true;
+								queue.add(new Integer[] {nr, nc});
+							}
+						}
+					}
+					dist1++;
+				}
+			}
+		}
+	}
+	
 }
